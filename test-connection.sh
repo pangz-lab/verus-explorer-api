8135c872b2a48d7a4f651b9298ad1782ad029a7d
pangz <pangz.lab@gmail.com>
2024-07-11 23:12:27 +0900

    updated compose.yaml and the connection tester

--- test-connection.sh
+++ test-connection.sh
@@ -4,6 +4,6 @@ echo "++++++++++++++++++++++++++++++++++++++++++"
 echo " Make sure you're using the correct port! "
 echo "++++++++++++++++++++++++++++++++++++++++++"
 echo ""
-curl --location "http://localhost:${PORT}/api/blockchain/info" \
---header 'x-api-key: 12345' \
+curl --location "http://localhost:${PORT}/api/v1/blockchain/info" \
+--header 'x-api-key: dmVydXNkZXNrdG9wOnk4RDZZWGhBRms2alNoSGlSQktBZ1JDeDB0OVpkTWYyUzNLMG83ek44U28=' \
 --header 'Authorization: Bearer Basic dmVydXNkZXNrdG9wOnk4RDZZWGhBRms2alNoSGlSQktBZ1JDeDB0OVpkTWYyUzNLMG83ek44U28='
